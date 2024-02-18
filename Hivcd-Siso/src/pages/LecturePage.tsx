b12346fd4773aa10adcb1059d0edb38446b29918
Minseo Kang <rachel5640@naver.com>
2024-02-18 16:49:56 +0900

    feat: Lecture 섹션 구분

--- Hivcd-Siso/src/pages/LecturePage.tsx
+++ Hivcd-Siso/src/pages/LecturePage.tsx
@@ -6,16 +6,73 @@ const LecturePage = () => {
   return (
     <>
       <DesktopHeader />
-      <LecturePageBox />
+      <LecturePageBox>
+        <TimeTable>시간표</TimeTable>
+        <TextArea>
+          <div>dlsdjfsldkf</div>
+          <div>dlsdjfsldkf</div>
+          <div>dlsdjfsldkf</div>
+          <div>dlsdjfsldkf</div>
+          <div>dlsdjfsldkf</div>
+          <div>dlsdjfsldkf</div>
+          <div>dlsdjfsldkf</div>
+          <div>dlsdjfsldkf</div>
+          <div>dlsdjfsldkf</div>
+          <div>dlsdjfsldkf</div>
+          <div>dlsdjfsldkf</div>
+          <div>dlsdjfsldkf</div>
+          <div>dlsdjfsldkf</div>
+          <div>dlsdjfsldkf</div>
+          <div>dlsdjfsldkf</div>
+        </TextArea>
+      </LecturePageBox>
       <Footer />
     </>
   );
 };
 const LecturePageBox = styled.section`
-  width: 100vw;
+  display: grid;
+  overflow-y: hidden;
+  position: relative;
 
-  /* stylelint-disable-next-line order/properties-order */
+  width: 100vw;
+  height: 70rem;
+  margin-top: 16.9rem;
+  margin-bottom: 4.1rem;
   padding: 0 8.2rem;
+
+  background-color: red;
+
+  grid-column-gap: 1rem;
+  grid-template-columns: repeat(16, 1fr);
+`;
+
+const TimeTable = styled.section`
+  overflow-y: auto;
+
+  min-height: 60dvh;
+  border-top: 3px solid;
+
+  background-color: pink;
+  grid-column: span 10;
+`;
+
+const TextArea = styled.section`
+  overflow-y: auto;
+
+  height: auto;
+  border-top: 3px solid;
+
+  background-color: green;
+  grid-column: span 6;
+
+  & > div {
+    display: block;
+
+    height: 10rem;
+
+    background-color: white;
+  }
 `;
 
 export default LecturePage;
