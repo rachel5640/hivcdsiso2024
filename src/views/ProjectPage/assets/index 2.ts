501113f11a7909579958bd0c743bc9d8984da814
Minseo Kang <rachel5640@naver.com>
2024-03-07 03:12:23 +0900

    remove: 불필요한 파일 삭제

--- src/views/ProjectPage/assets/index 2.ts
+++
@@ -1,5 +0,0 @@
-import ExhibitionBtnSmall from './exhibition_btn_small.svg?react';
-import WorksBtnSmall from './works_btn_small.svg?react';
-
-export { ExhibitionBtnSmall };
-export { WorksBtnSmall };
